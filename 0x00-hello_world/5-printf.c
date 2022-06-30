763d202721da21838f2940c4c26853bcbd15b7c3
fasika <fasikafikadu38@gmail.com>
2022-06-30 07:56:48 -0700

    Hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
 * main - prints the size of various types
 * Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
-/*main printing*/
-puts("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
