e1375cee5cca70aef50d7a238c4273c61fd0d318
fasika <fasikafikadu38@gmail.com>
2022-06-30 07:33:43 -0700

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include <stdio>
+#include <stdio.h>
 /*main prints the given sentence*/
 int main(void)
 {
+/*main printing*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
