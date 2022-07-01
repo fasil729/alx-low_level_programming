d87baa38db59a1797c72b7247ceed3264a9d55d6
fasika <fasikafikadu38@gmail.com>
2022-07-01 09:54:48 -0700

    hot fox

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+/**
+*main assigns numbers to their type
+*it returns 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+    printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+    printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+    printf("%d is negative\n", n);
+}
+return (0);	
+}
