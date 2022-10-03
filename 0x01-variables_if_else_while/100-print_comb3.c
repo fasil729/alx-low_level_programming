94262fe08e897874558ffd95b26caab520322c41
fasika <fasikafikadu38@gmail.com>
2022-10-03 09:12:20 -0700

    advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ * Description: print all differnt combinations of two digits
+ * Return: always 0(success)
+ */
+int main(void)
+{
+    int i = 1;
+
+    while (i < 90)
+    {
+        if (i < 10)
+        {
+            putchar(0);
+            putchar(i);
+            i++;
+        }
+        if (i % 10 == 0)
+        {
+            int mod = i / 10;
+            
+            mod = mod % 10;
+            i += mod + 1;
+            while (i < i+10) 
+            {
+                putchar(i);
+                i++;
+            }
+        }
+        if (i != 89)
+        {
+            putchar(",");
+            putchar(" ");
+        } 
+    }
+    return(0);
+}
