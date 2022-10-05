16830db2128e656b1099b3d0ebae33d434d0cf24
fasika <fasikafikadu38@gmail.com>
2022-10-04 19:33:11 -0700

    advanced 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 /**
  * main - Entry Point
  * Description: print all differnt combinations of two digits
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: print all differnt combinations of two digits
+ * Return: always 0(success)
+ */
+int main(void)
+{
+        for (int i = 0; i < 8; i++)
+        {
+                for (int j = i + 1; j < 9; j++)
+                {
+                        for (int k = j + 1; k < 10; k++)
+                        {
+                                putchar(i + '0');
+                                putchar(j + '0');
+                                putchar(k + '0');
+                                if (i == 7 && j == 8 && k == 9)
+                                {
+                                        putchar('\n');
+                                        continue;
+                                }
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+
+        }
+        return (0);
+}
