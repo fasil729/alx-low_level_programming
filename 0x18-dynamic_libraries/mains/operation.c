0dec4c8fe42e90af6e20dcb306f6d0b46c7c15a4
fasika <fasikafikadu38@gmail.com>
2022-10-08 03:06:28 -0700

    C in Python

---
+++ 0x18-dynamic_libraries/mains/operation.c
@@ -0,0 +1,41 @@
+/**
+ * add->add two numbers a and b
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+   return(a + b);
+}
+/**
+ * sub->substruct  two numbers a and b
+ * Return: reuslt of substraction a and b
+ */
+int sub(int a, int b)
+{
+   return(a - b);
+}
+/**
+ * mul->multiply two numbers a and b
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+   return(a * b);
+}
+/**
+ * div->divide two numbers a and b
+ * Return: quotient of a and b
+ */
+int div(int a, int b)
+{
+   return(a / b);
+}
+/**
+ * mod->caluculate the remainder  of two numbers a and b
+ * Return: modulo of a and b
+ */
+int mod(int a, int b)
+{
+   return(a % b);
+}
