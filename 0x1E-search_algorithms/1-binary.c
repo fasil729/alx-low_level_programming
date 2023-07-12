b327fe74c282fd52b66b60eb6d8c871dcaf60004
fasika <fasikafikadu38@gmail.com>
2023-07-12 11:19:30 +0300

    fix error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,14 +13,14 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0, right = size - 1, mid;
+    size_t left = 0, right = size - 1, mid, i;
 
     if (array == NULL)
         return (-1);
 
     while (left <= right)
     {
-        mid = (left + right) / 2, size_t i;
+        mid = (left + right) / 2;
 
         printf("Searching in array: ");
         for (i = left; i <= right; i++)
