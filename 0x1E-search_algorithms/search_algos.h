25def0e184ed4d1470f3332e1ad7eb796cf4b7b3
fasika <fasikafikadu38@gmail.com>
2023-07-12 11:09:30 +0300

    add prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,10 @@
 #define SEARCH_ALGOS_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /* Function prototypes */
+int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
