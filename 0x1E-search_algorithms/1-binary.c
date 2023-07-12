9ffc2577af8b9cf6009e10c2f8713ef15a4cc680
fasika <fasikafikadu38@gmail.com>
2023-07-12 11:17:17 +0300

    fix size

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 
     while (left <= right)
     {
-        mid = (left + right) / 2, size_t i = left;
+        mid = (left + right) / 2, size_t i;
 
         printf("Searching in array: ");
         for (i = left; i <= right; i++)
